b852a9e32045b9ac326b3562f5c0513321642d45
binsidh <58246686+binsidh@users.noreply.github.com>
2022-05-04 15:18:54 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,55 @@
+song = "";
+
+function preload()
+{
+	song = loadSound("music.mp3");
+}
+
+
+scoreLeftWrist = 0;
+
+rightWristX = 0;
+rightWristY = 0;
+
+leftWristX = 0;
+leftWristY = 0;
+
+function setup() {
+	canvas =  createCanvas(600, 500);
+	canvas.center();
+
+	video = createCapture(VIDEO);
+	video.hide();
+
+	poseNet = ml5.poseNet(video, modelLoaded);
+	poseNet.on('pose', gotPoses);
+}
+
+function modelLoaded() {
+  console.log('PoseNet Is Initialized');
+}
+
+
+function draw() {
+	image(video, 0, 0, 600, 500);
+
+	fill("#FF0000");
+	stroke("#FF0000");
+
+
+		circle(rightWristX,rightWristY,20);
+
+
+
+	if(scoreLeftWrist > 0.2)
+	{
+		circle(leftWristX,leftWristY,20);
+		InNumberleftWristY = Number(leftWristY); 
+		remove_decimals = floor(InNumberleftWristY);
+		volume = remove_decimals/500;
+		document.getElementById("volume").innerHTML = "Volume = " + volume;		
+		song.setVolume(volume);	
+	}
+
+}
+
